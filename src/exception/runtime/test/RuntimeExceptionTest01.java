24c3ea270e9c37f01bbfe88b31ee44a95f2c6261
yami-r7 <rafaelyami7@hotmail.com>
2025-10-08 21:15:21 -0300

    Exceptions

---
+++ src/exception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package exception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        //Erros do programador
+        int[] nums = {1, 2};
+        System.out.println(nums[2]);
+    }
+}
