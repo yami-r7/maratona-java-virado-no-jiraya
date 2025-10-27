7525e293d5dd154f254190670fe309c2a488627d
yami-r7 <rafaelyami7@hotmail.com>
2025-10-27 13:24:46 -0300

    Wrappers pt 01

---
+++ src/wrapper/test/WrapperTest01.java
@@ -0,0 +1,23 @@
+package wrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 127;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+    }
+}
