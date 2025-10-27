846c9a223a81537a039f3d4ca9a37716fe1f0351
yami-r7 <rafaelyami7@hotmail.com>
2025-10-27 13:42:49 -0300

    Wrappers pt 02

--- src/wrapper/test/WrapperTest01.java
+++ src/wrapper/test/WrapperTest01.java
@@ -13,11 +13,24 @@ public class WrapperTest01 {
 
         Byte byteW = 127;
         Short shortW = 1;
-        Integer intW = 1;
+        Integer intW = 1; //autoboxing
         Long longW = 10L;
         Float floatW = 10F;
         Double doubleW = 10D;
         Character charW = 'W';
         Boolean booleanW = false;
+
+        int i = intW;
+        Integer intW2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean(null);
+        System.out.println(verdadeiro);
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
     }
 }
