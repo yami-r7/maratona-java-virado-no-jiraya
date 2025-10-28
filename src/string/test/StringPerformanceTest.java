41c471f8d91ec24068ef87fd15cacd8c9222e61a
yami-r7 <rafaelyami7@hotmail.com>
2025-10-28 13:49:27 -0300

    Strings pt 03 - Desempenho

---
+++ src/string/test/StringPerformanceTest.java
@@ -0,0 +1,41 @@
+package string.test;
+
+public class StringPerformanceTest {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(30_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuilder " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuffer " + (fim - inicio) + "ms");
+    }
+
+    private static void concatString(int tamanho) {
+        String texto = "Olá, meu nome é Rafael";
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+        }
+    }
+
+    private static void concatStringBuilder(int tamanho) {
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+
+    private static void concatStringBuffer(int tamanho) {
+        StringBuffer sb = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+}
