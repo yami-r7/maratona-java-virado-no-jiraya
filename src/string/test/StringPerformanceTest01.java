2c7ecffbb15aeb2b6ba27d3c2d0f00aef4481216
yami-r7 <rafaelyami7@hotmail.com>
2025-10-29 13:05:26 -0300

    Rename StringPerformanceTest01

--- src/string/test/StringPerformanceTest01.java
+++ src/string/test/StringPerformanceTest01.java
@@ -1,6 +1,6 @@
 package string.test;
 
-public class StringPerformanceTest {
+public class StringPerformanceTest01 {
     public static void main(String[] args) {
         long inicio = System.currentTimeMillis();
         concatString(30_000);
