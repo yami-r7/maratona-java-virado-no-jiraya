5b193522c5a17c2d19b42cbab5f0c2305f9aeb63
yami-r7 <rafaelyami7@hotmail.com>
2025-11-18 21:32:03 -0300

    IO pt 02 - FileWriter

---
+++ src/io/test/FileWriter01.java
@@ -0,0 +1,23 @@
+package io.test;
+
+// File
+// FileWriter
+// FileReader
+// BufferedWrite
+// BufferedReader
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true)) {
+            fw.write("O DevDojo é o melhor curso do Brasil\nCurso Maratona Java");
+            fw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
