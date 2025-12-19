362688fad746855de4a2ecab2b3ded3b2fb8608a
yami-r7 <rafaelyami7@hotmail.com>
2025-12-19 16:35:38 -0300

    Coleções pt 02 - equals pt 02

--- src/colecoes/test/Smartphone.java
+++ src/colecoes/test/Smartphone.java
@@ -9,6 +9,15 @@ public class Smartphone {
         this.marca = marca;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
