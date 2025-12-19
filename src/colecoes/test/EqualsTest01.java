f26698912b2433a879fad13bb2f466958f12415e
yami-r7 <rafaelyami7@hotmail.com>
2025-12-19 16:10:28 -0300

    Coleções pt 01 - equals pt 01

---
+++ src/colecoes/test/EqualsTest01.java
@@ -0,0 +1,10 @@
+package colecoes.test;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("1ABC1", "iPhone");
+        Smartphone s2 = new Smartphone("1ABC1", "iPhone");
+
+        System.out.println(s1.equals(s2));
+    }
+}
