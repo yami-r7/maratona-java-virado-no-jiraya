3ecb5c3b6191ff51a6cc4d30ca4f55b5a1afb3dd
yami-r7 <rafaelyami7@hotmail.com>
2025-12-20 22:30:59 -0300

    Coleções pt 07 - List pt 02

--- src/colecoes/test/ListTest01.java
+++ src/colecoes/test/ListTest01.java
@@ -5,19 +5,25 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-        List<String> nomes = new ArrayList(16);
+        List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
         nomes.add("Rafael");
-        nomes.add("DevDojo Academy");
+        nomes.add("DevDojo");
+        nomes2.add("Yamí");
+        nomes2.add("Academy");
+
+        nomes.addAll(nomes2);
 
         for (Object nome : nomes) {
             System.out.println(nome);
         }
 
-        nomes.add("Yamí");
-
         System.out.println("-------------");
         for (int i=0; i<nomes.size(); i++) {
             System.out.println(nomes.get(i));
         }
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
     }
 }
