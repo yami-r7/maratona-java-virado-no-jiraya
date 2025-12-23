61905200818efaa2b3a86cb8be1b62e5932663a2
yami-r7 <rafaelyami7@hotmail.com>
2025-12-22 22:44:32 -0300

    Coleções pt 09 - Sorting lists pt 01

---
+++ src/colecoes/test/ListSortTest01.java
@@ -0,0 +1,38 @@
+package colecoes.test;
+
+import colecoes.dominio.Time;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListSortTest01 {
+    public static void main(String[] args) {
+        List<String> games = new ArrayList<>(6);
+        games.add("Minecraft");
+        games.add("Clash Royalle");
+        games.add("GTA SA");
+        games.add("Rocket League");
+        games.add("Eatventure");
+
+        Collections.sort(games);
+
+        for (String game : games) {
+            System.out.println(game);
+        }
+
+        List<Double> dinheiros = new ArrayList<>();
+        dinheiros.add(21.99);
+        dinheiros.add(45.77);
+        dinheiros.add(98.49);
+        dinheiros.add(03.98);
+
+        System.out.println(dinheiros);
+
+        Collections.sort(dinheiros);
+
+        System.out.println(dinheiros);
+
+        new Time(null, null, 0);
+    }
+}
